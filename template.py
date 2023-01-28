e08f2d33d0cdc053400711fb37bd33c075787f6e
Ranjit singh <jiradhey402@gmail.com>
2023-01-28 15:26:18 +0530

    project template build

--- template.py
+++ template.py
@@ -1,23 +1,22 @@
 import os
 from pathlib import Path
 import logging
-
+# define the logging object
+formt = "%(lineno)d--%(name)s--%(asctime)s--%(levelname)s--%(message)s"
 logging.basicConfig(
     level = logging.INFO,
-    format = "[%(asctime)s: %(levelname)s]: %(message)s"
+    format = formt
 )
-
+# to enter the project name
 while True:
     project_name = input("Enter your project name: ")
     if project_name !='':
         break
-
 logging.info(f"Creating project by name: {project_name}")
 
 list_of_files = [
-    ".github/workflows/.gitkeep",
+    ".github/workflows/.gitkeep",    # to keep unnecessary data. just like .gitignore
     ".github/workflows/main.yaml",
-   # f"src/{project_name}/__init__.py",
     f"{project_name}/__init__.py",
     f"{project_name}/components/__init__.py",
     f"{project_name}/entity/__init__.py",
