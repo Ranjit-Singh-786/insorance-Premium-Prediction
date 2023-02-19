8d400994c8b6b32547ba39e4930fec27f74bb4f1
Ranjit singh <jiradhey402@gmail.com>
2023-02-19 21:24:50 +0530

    model pusher successfully completed

--- insurance/components/model_evaluation.py
+++ insurance/components/model_evaluation.py
@@ -56,26 +56,26 @@ class ModelEvaluation:
             target_encoder = load_model(file_path=traget_encoder_path)
 
             # to get the current model
-            current_transformer = load_model(file_path=self.data_transformation_artifact.transform_object_path)
-            current_model = load_model(file_path=self.model_trainer_artifact.model_path)
+            current_transformer = load_model(file_path=self.data_transformation_artifact.transformer_object_path)
+            current_model = load_model(file_path=self.model_trainer_artifact.model_file_path)
             current_target_encoder = load_model(file_path=self.data_transformation_artifact.target_encoder_path)
 
             # get the transform_test  data. to evaluate both model, saved_model Vs current model
             trasnform_test_data = utils.load_transformed_data(file_path=self.data_transformation_artifact.transformed_test_path)
-            input_features_name = list(transformer.feature_names_in_)
+            
+            trasnform_test_data = pd.DataFrame(trasnform_test_data)
+            column = ['age', 'sex', 'bmi', 'children', 'smoker', 'region', 'charges']
+            trasnform_test_data.columns = column
+            input_features_name = os.getenv('INPUT_FEATURE_NAME').split(',')
             target_df = trasnform_test_data[TARGET_COLUMN]
+            
             input_arr = trasnform_test_data[input_features_name]
             y_true = target_df
             # previous model testing
             y_pred = model.predict(input_arr)
             previous_model_score = r2_score(y_true=y_true, y_pred=y_pred)
             logging.info(f"accuracy of saved_model model on transform test data :- {previous_model_score}")
-            #PREVIOS transformer MODEL from saved_model
-            # for i in input_features_name:
-            #     if test_df[i].dtypes == 'object':
-            #         # labelencoding
-            #         test_df[i] = target_encoder.transform(test_df[i])
-            # input_arr = transformer.transform(test_df[input_features_name])
+
 
             # Accuracy of current model
 
@@ -90,9 +90,12 @@ class ModelEvaluation:
             # FInal comparision between both model
             if current_model_score <= previous_model_score:
                 logging.info(f"Current trained model is not better than previous model")
-                raise Exception("Current model is not better than previous model")
+                print("Current model is not better than previous model")
+            else:
+                print('current model is batter than previous model !')
+                logging.info(f"current model is batter than previous model !")
 
-
+            logging.info(f"evaluated both model on transformed test data")
             model_eval_artifact = artifact_entity.ModelEvaluationArtifact(is_model_accepted =  True, 
             improved_accuracy=current_model_score - previous_model_score)
             logging.info(f"current_model_score - previous_model_score = {current_model_score - previous_model_score}")
--- insurance/predictor.py
+++ insurance/predictor.py
@@ -1,6 +1,7 @@
-import os
+import os,sys
 from typing import Optional
 from insurance.entity.config_entity import TRANSFORMER_OBJECT_FILE_NAME,MODEL_NAME,TARGET_ENCODER_OBJECT_FILE_NAME
+from insurance.exception import InsuranceException
 
 
 class ModelResolver:
@@ -29,7 +30,7 @@ class ModelResolver:
             return os.path.join(self.model_registry, f"{latest_dir_name}")
 
         except Exception as e:
-            raise e
+            raise InsuranceException(e,sys)
 # 2
 
     def get_latest_model_path(self):
@@ -40,7 +41,7 @@ class ModelResolver:
 
             return os.path.join(latest_dir, self.model_dir_name, MODEL_NAME)
         except Exception as e:
-            raise e
+            raise InsuranceException(e,sys)
 # 3
 
     def get_latest_transformer_path(self):
@@ -49,9 +50,9 @@ class ModelResolver:
             if latest_dir is None:
                 raise Exception(f"Transform data is not avaliable")
 
-            return os.path.join(latest_dir, self.transformer_dir_name,TRANSFORMER_OBJECT_FILE_NAME)
+            return os.path.join(latest_dir, self.transfomer_dir_name,TRANSFORMER_OBJECT_FILE_NAME)
         except Exception as e:
-            raise e
+            raise InsuranceException(e,sys)
 
 # 4
     def get_latest_target_encoder_path(self):
@@ -62,20 +63,25 @@ class ModelResolver:
 
             return os.path.join(latest_dir, self.traget_encoder_dir_name, TARGET_ENCODER_OBJECT_FILE_NAME)
         except Exception as e:
-            raise e
+            raise InsuranceException(e,sys)
         
     # if empty then created path for "0" otherwise path for +1
     def get_latest_save_dir_path(self)->str:
 
         try:
             latest_dir = self.get_latest_dir_path()
+            # os.makedirs(latest_dir,exist_ok=True)
             if latest_dir ==  None:
+                # return latest_dir
+
                 return os.path.join(self.model_registry, f"{0}")
             # if it is not None
+            
             latest_dir_num = int(os.path.basename(self.get_latest_dir_path())) #basename --> last location of this path
-            return os.path.join(self.model_registry, f"{latest_dir_num + 1}") ## add 1 so that it will increase everytime
+            latest_dir  = os.path.join(self.model_registry, f"{latest_dir_num+1}") ## add 1 so that it will increase everytime
+            return latest_dir 
         except Exception as e:
-            raise e
+            raise InsuranceException(e,sys)
 # 6
 
     def get_latest_save_model_path(self):
@@ -83,14 +89,14 @@ class ModelResolver:
             latest_dir = self.get_latest_save_dir_path()
             return os.path.join(latest_dir, self.model_dir_name, MODEL_NAME) # model.pkl
         except Exception as e:
-            raise e
+            raise InsuranceException(e,sys)
 # 7
     def get_latest_save_transfomer_path(self):
         try:
             latest_dir = self.get_latest_save_dir_path()
             return os.path.join(latest_dir, self.transfomer_dir_name, TRANSFORMER_OBJECT_FILE_NAME) # transform.pkl
         except Exception as e:
-            raise e
+            raise InsuranceException(e,sys)
 
 # 8
     def get_latest_save_target_encoder_path(self):
@@ -98,4 +104,4 @@ class ModelResolver:
             latest_dir = self.get_latest_save_dir_path()
             return os.path.join(latest_dir, self.traget_encoder_dir_name, TARGET_ENCODER_OBJECT_FILE_NAME) # encoder.pkl
         except Exception as e:
-            raise e
+            raise InsuranceException(e,sys)
